9cde37ea242f3ed1ff09d3a1579985939a631f94
John Kristoffer Stene Armstrong <jkr.armstrong@gmail.com>
2025-06-16 14:06:25 +0200

    Fixed logo.

--- app/data.py
+++ app/data.py
@@ -2,5 +2,16 @@
 import pandas as pd
 
 # Read the CSV-file
-df = pd.read_csv("data/renset_jentenavn.csv", sep=",", encoding="utf-8")
-df.columns = df.columns.str.strip().str.lower()
+df_girl = pd.read_csv("data/renset_jentenavn.csv", sep=",", encoding="utf-8")
+df_boy = pd.read_csv("data/renset_guttenavn.csv", sep=",", encoding="utf-8")
+
+# --- Standardize column names ---
+df_girl.columns = df_girl.columns.str.strip().str.lower()
+df_boy.columns = df_boy.columns.str.strip().str.lower()
+
+# --- Add gender column (useful for later) ---
+# df_girl["gender"] = "girl"
+# df_boy["gender"] = "boy"
+
+# --- Combine datasets ---
+df = pd.concat([df_girl, df_boy], ignore_index=True)
